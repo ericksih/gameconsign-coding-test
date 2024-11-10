37fdf0a4631e3dc4c437dd78b1d6f9c2336bf5ca
Erick S. Hasibuan <80398690+ericksih@users.noreply.github.com>
2024-11-10 22:06:29 +0700

    fix: task 4: Logout from the session, Change the login page to use form generator with the `useFormGen`

--- src/components/form/FormFields.tsx
+++ src/components/form/FormFields.tsx
@@ -1,46 +1,43 @@
 import React from "react";
-import {FormFieldProps} from "@/components/form/types";
-import {TextField} from "@/components/form/fields/TextField";
-import {FormLabel} from "@/components/form/FormLabel";
+import { FormFieldProps } from "@/components/form/types";
+import { TextField } from "@/components/form/fields/TextField";
+import { FormLabel } from "@/components/form/FormLabel";
+import { PasswordField } from "./fields/PasswordField";
 
-const FormField
-    = React.forwardRef<HTMLDivElement, FormFieldProps>((
-    {
-        field,
-        path,
-        value,
-        errors,
-        updateModelValue,
-        ...props
-    },
-    ref
-) => {
+const FormField = React.forwardRef<HTMLDivElement, FormFieldProps>(
+  ({ field, path, value, errors, updateModelValue, ...props }, ref) => {
+    // Determines which component to render based on field type
     const generateField = () => {
-        let Component = null;
+      let Component = null;
 
-        switch (field.type) {
-            case "text":
-                Component = TextField;
-                break;
-            default:
-                return <></>;
-        }
+      switch (field.type) {
+        case "text":
+          Component = TextField;
+          break;
+        case "password":
+          Component = PasswordField;
+          break;
+        default:
+          return null;
+      }
 
-        return (
-            <FormLabel label={field.label} errors={errors} path={path}>
-                <Component
-                    field={field}
-                    // @ts-ignore
-                    value={value}
-                    path={path}
-                    updateModelValue={updateModelValue}
-                />
-            </FormLabel>
-        )
-    }
-    return generateField();
-});
+      return (
+        <FormLabel label={field.label} errors={errors} path={path}>
+          <Component
+            field={field}
+            value={value as string}
+            path={path}
+            updateModelValue={updateModelValue}
+            {...props}
+          />
+        </FormLabel>
+      );
+    };
 
-FormField.displayName = 'FormField';
+    return <div ref={ref}>{generateField()}</div>;
+  }
+);
 
-export {FormField};
+FormField.displayName = "FormField";
+
+export { FormField };
